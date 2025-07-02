2886626e29b3612162453f9e2739d9a8de304a67
jardison2021 <jardison@live.com>
2025-07-01 21:06:57 -0300

    App lista de tarefas - inserindo registros - parte 6

--- tarefa_controller.php
+++ tarefa_controller.php
@@ -1,5 +1,20 @@
 <?php
+	require "../../app_lista_tarefas/tarefa.model.php";
+	require "../../app_lista_tarefas/tarefa.service.php";
+	require "../../app_lista_tarefas/conexao.php";
+	//teste
 	echo '<pre>';
 	print_r($_POST);
+	echo '</pre>';
+	//instância do objeto tarefa
+	$tarefa = new Tarefa();
+	$tarefa->__set('tarefa', $_POST['tarefa']);
+	//instância de conexão
+	$conexao = new Conexao();
+	//instância de tarefa service
+	$tarefaService = new TarefaService($conexao, $tarefa);
+	$tarefaService->inserir();
+	echo '<pre>';
+	print_r($tarefaService);
 	echo '</pre>';
 ?>
