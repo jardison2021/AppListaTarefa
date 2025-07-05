b81e7d1fcabebabb07e3947ffd6efec6b1107c99
jardison2021 <jardison@live.com>
2025-07-04 21:43:38 -0300

    App lista de tarefas - listando todos os registros - parte 8

--- tarefa_controller.php
+++ tarefa_controller.php
@@ -3,13 +3,18 @@
 	require "../../app_lista_tarefas/tarefa.service.php";
 	require "../../app_lista_tarefas/conexao.php";
 	//instância do objeto tarefa
-	$tarefa = new Tarefa();
-	$tarefa->__set('tarefa', $_POST['tarefa']);
-	//instância de conexão
-	$conexao = new Conexao();
-	//instância de tarefa service
-	$tarefaService = new TarefaService($conexao, $tarefa);
-	$tarefaService->inserir();
-	//Método header
-	header('Location: nova_tarefa.php?inclusao=1');
+	$acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
+	if($acao == 'inserir'){;
+		$tarefa = new Tarefa();
+		$tarefa->__set('tarefa', $_POST['tarefa']);
+		//instância de conexão
+		$conexao = new Conexao();
+		//instância de tarefa service
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefaService->inserir();
+		//Método header
+		header('Location: nova_tarefa.php?inclusao=1');
+	} else if($acao == 'recuperar'){
+		echo 'Chegramos até aqui!';
+	}
 ?>
